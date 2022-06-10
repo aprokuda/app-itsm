9a19c2ef2642e90fa1908129c323b64416796195
Kana <jkanatbek97@gmail.com>
2022-06-10 09:46:27 +0600

    Добавление проверки порта при запуске сервера для логов

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const template = require('./templateResponse');
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ limit: '50kb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '50kb' }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'upload')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
@@ -20,11 +21,28 @@ db.sequelize.sync();
 //    console.log('Altered and Re-synced Database');
 // });
 
+////////////////////    ROUTES      //////////////////////
 require('./routes/auth.route')(app);
 require('./routes/user.route')(app);
 require('./routes/role.route')(app);
 require('./routes/request.route')(app);
 
+app.use((req, res) => { template(404, "Route '" + req.url + "' not found!", [], false, res); })
+
+// if (process.env.HOST_PORT == 'development') {
+//   app.listen(PORT, () =>
+//     console.log(`Example auth is listening on port ${PORT}!`, Date())
+//   )
+// } else if (NODE_ENV == 'production') {
+//   let httpsOptions = {
+//     key: fs.readFileSync('/home/askar/certs/m-lombard.key'),
+//     cert: fs.readFileSync('/home/askar/certs/m-lombard.crt')
+//   }
+//   const server = https.createServer(httpsOptions, app).listen(PORT, () => {
+//     console.log(`App auth is running on port ${PORT}...`, Date())
+//   })
+// }
+
 app.listen(process.env.HOST_PORT, () => {
-    console.log("Server start!")
+    console.log("Server started on port " + process.env.HOST_PORT + "!", Date());
 });
